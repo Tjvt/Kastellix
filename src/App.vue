0db9e814de4df65d355bcaf2869032a9b28ad173
tjvt <janmehlem@gmail.com>
2026-05-11 12:36:51 +0200

    add Tailwind CSS setup

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="bg-gray-100 min-h-screen p-8">
+    <img alt="Vue logo" src="./assets/logo.png" class="mx-auto">
+    <HelloWorld msg="Welcome to Your Vue.js App" class="mt-8 text-blue-600 font-bold"/>
+  </div>
 </template>
 
 <script>
